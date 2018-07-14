66ff6ac2bc9ae8a42491083df7c5755d5410a425
tylee <michaelgod5@gmail.com>
2018-07-13 22:32:43 -0700

    Add setup.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mnist_data_processor
 MNIST IDX Data Processing Kit
+
+This is a small tool kit to process raw MNIST database form [Yann Lecun's webpage](http://yann.lecun.com/exdb/mnist/)
+
+## Quick start
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MNIST Data Processor',
+      version='0.1',
+      description='A small toolkit to process raw MNIST handwriting database in IDX format',
+      url='https://github.com/taoyilee/mnist_data_processor',
+      author='Michael (Tao-Yi) Lee',
+      author_email='taoyil@uci.edu',
+      license='MIT',
+      packages=['app'],
+      zip_safe=False)
